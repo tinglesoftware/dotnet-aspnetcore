fa79963bbcce71c5e28e4e1ae01ade26e3051a59
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-10 09:36:17 +0300

    Migrate examples to minimal hosting

--- samples/DataProtectionMongoDBSample/Program.cs
+++ samples/DataProtectionMongoDBSample/Program.cs
@@ -1,16 +1,6 @@
-namespace DataProtectionMongoDBSample;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+app.MapGet("/", () => "Hello World!");
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+app.Run();
