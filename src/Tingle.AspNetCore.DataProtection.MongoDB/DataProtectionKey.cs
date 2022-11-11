225d7a05d757a7414c9dba1184a3e11591e507ea
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-11 08:30:21 +0300

    Enable NRT in Tingle.AspNetCore.DataProtection.MongoDB

--- src/Tingle.AspNetCore.DataProtection.MongoDB/DataProtectionKey.cs
+++ src/Tingle.AspNetCore.DataProtection.MongoDB/DataProtectionKey.cs
@@ -17,10 +17,10 @@ public class DataProtectionKey
     /// <summary>
     /// The friendly name of the <see cref="DataProtectionKey"/>.
     /// </summary>
-    public string FriendlyName { get; set; }
+    public string? FriendlyName { get; set; }
 
     /// <summary>
     /// The XML representation of the <see cref="DataProtectionKey"/>.
     /// </summary>
-    public string Xml { get; set; }
+    public string? Xml { get; set; }
 }
